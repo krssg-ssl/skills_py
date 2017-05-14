38032b160010f4c455ba8dc8c64eefdbc3b814cf
ashishkg0022 <ashishkg0022@gmail.com>
2017-05-15 00:45:58 +0530

    completed SDribbleTurn

--- scripts/skills/sDribbleTurn.py
+++ scripts/skills/sDribbleTurn.py
@@ -1,7 +1,35 @@
 import skill_node
-
+FRICTION_COEFF = 0.05
+ACCN_GRAVITY = 9.80665
+ANGLE_THRES =30.0
 def execute(param, state, bot_id):
-	point = Vector2D(param.DribbleTurnP.x, param.DribbleTurnP.y)
-	skill_node.send_command(state.isteamyellow, bot_id, 0, 0, 0, 0, True)	
+    point = Vector2D(param.DribbleTurnP.x, param.DribbleTurnP.y)
+    botPos = Vector2D(state.homePos[bot_id].x, state.homePos[bot_id].y)
+    ballPos = Vector2D(state.ballPos.x, state.ballPos.y)
+    radius = param.DribbleTurnP.turn_radius
+    ob = Vector2D()
+    finalSlope = ob.angle(point, ballPos)
+    turnAngleLeft = ob.normalizeAngle(finalSlope - state.homePos[bot_id].theta)
 
-print(1)
+    if turnAngleLeft>=-ANGLE_THRES*PI/180 and turnAngleLeft<=ANGLE_THRES*PI/180 :
+        omega = ((turnAngleLeft)*180/ (ANGLE_THRES*PI)) * param.DribbleTurnP.max_omega
+    else :
+        omega=param.DribbleTurnP.max_omega*(-1 if turnAngleLeft < 0  else 1 )
+        
+    phi = ob.normalizeAngle(atan2(omega*omega*radius,FRICTION_COEFF*ACCN_GRAVITY))
+   
+    sphi=sin(phi)
+    cphi=cos(phi)
+
+    if omega >= 0 :
+        v_x= omega*radius*sphi
+        v_y= -omega*radius*cphi
+    else :
+        v_x= -omega*radius*sphi
+        v_y= -omega*radius*cphi
+
+        
+
+        
+
+    skill_node.send_command(state.isteamyellow, botID, v_x, v_y, omega, 0, true)
