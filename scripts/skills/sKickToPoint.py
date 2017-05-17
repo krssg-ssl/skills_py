95c825c492332a5e841a6fac123ff75d4656a673
Ashish Kumar Gaurav <ashishkg0022@gmail.com>
2017-05-17 18:31:55 +0530

    corrected few errors (#19)

--- scripts/skills/sKickToPoint.py
+++ scripts/skills/sKickToPoint.py
@@ -3,7 +3,7 @@ import maths
 import sGoToBall
 import sTurnToPoint
 def execute(param, state, bot_id):
-	botPos=Vector2D(state.homePos[bot_id].x, state.homePos[bot_id].y)
+    botPos=Vector2D(state.homePos[bot_id].x, state.homePos[bot_id].y)
     ballPos=Vector2D(state.ballPos.x, state.ballPos.y)
     destPoint=Vector2D(param.KickToPointP.x, param.KickToPointP.y)
     ob = Vector2D()
@@ -13,14 +13,14 @@ def execute(param, state, bot_id):
     dist = ob.dist(ballPos, botPos)
 
     if dist > BOT_BALL_THRESH :
-        return  sGoToBall.execute(param,state,bot_id)
+        sGoToBall.execute(param,state,bot_id)
 
     if math.fabs(turnAngleLeft) > SATISFIABLE_THETA/2 : # SATISFIABLE_THETA in config file
-        Sparam paramt()
+        paramt=Sparam()
         paramt.TurnToPointP.x = destPoint.x
         paramt.TurnToPointP.y = destPoint.y
         paramt.TurnToPointP.max_omega = MAX_BOT_OMEGA*3
-        return sTurnToPoint.execute(paramt, state, bot_id)
+        sTurnToPoint.execute(paramt, state, bot_id)
 
     skill_node.send_command(state.isteamyellow, bot_id ,0, 0, 0, param.KickToPointP.power, False)
 
