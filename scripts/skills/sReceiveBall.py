77a9a79094fb600b020f8f506f4897f8002c0a21
americast <sayan.sinha@iitkgp.ac.in>
2017-05-22 22:01:37 +0530

    fix

--- scripts/skills/sReceiveBall.py
+++ scripts/skills/sReceiveBall.py
@@ -3,13 +3,14 @@ import skill_node
 def execute(param, state, bot_id):
      ballInitialpos=Vector2D()
      ballFinalpos=Vector2D()
-     currentBotpos = Vector2D(state.homePos, botDestination)
+     currentBotpos = Vector2D(int(state.homePos[bot_id].x),int(state.homePos[bot_id].y))
+     botDestination = Vector2D
      if(execute.framecount == 1):
-       ballInitialpos = state.ballPos
+       ballInitialpos = Vector2D(state.ballPos.x,state.ballPos.y)
        return
      elif((execute.framecount % 10) == 0):
        execute.framecount = 1
-       ballFinalpos = state.ballPos
+       ballFinalpos = Vector2D(state.ballPos.x,state.ballPos.y)
      else:
        execute.framecount+=1
        return
@@ -21,10 +22,12 @@ def execute(param, state, bot_id):
 
      botDestination_x = -(a*c1 + b*c2)/(a*a + b*b)
      botDestination_y = (a*c2 - b*c1)/(a*a + b*b)
+     '''
      param.GoToPointP.x = botDestination_x
      param.GoToPointP.y = botDestination_y
      param.GoToPointP.finalslope = 0
      param.GoToPointP(param)
+     '''
      
      skill_node.send_command(state.isteamyellow, bot_ID, 0, 0, 0, 0, False)
 	
